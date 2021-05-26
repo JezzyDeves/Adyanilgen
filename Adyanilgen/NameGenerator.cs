68a68ff71901aeb06da427027986005e1b0cfadd
JezzyDeves <17914687+JezzyDeves@users.noreply.github.com>
2021-05-25 21:52:48 -0400

    Began working on syllable structures

--- Adyanilgen/NameGenerator.cs
+++ Adyanilgen/NameGenerator.cs
@@ -4,9 +4,29 @@ namespace Adyanilgen
 {
     public class NameGenerator
     {
-        public void GenerateVoid()
+        Phonetics _phonetics = new Phonetics();
+        Random GetRandom = new Random();
+        public string BasicSyllable()
         {
+            string syllable = "";
 
+            char[] syllableStructure = new char[] { 'c', 'v', 'c', 'c' };
+
+            foreach (var item in syllableStructure)
+            {
+                if (item.Equals('c'))
+                {
+                    var index = GetRandom.Next(_phonetics.Consanants.Count);
+                    syllable += _phonetics.Consanants[index];
+                }
+                else
+                {
+                    var index = GetRandom.Next(_phonetics.Vowels.Count);
+                    syllable += _phonetics.Vowels[index];
+                }
+            }
+
+            return syllable;
         }
     }
 }
---
+++ Adyanilgen/Phonetics.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adyanilgen
+{
+    /// <summary>
+    /// This class is where the phonetics used in the generation of words are stored
+    /// </summary>
+    public class Phonetics
+    {
+        public List<string> Vowels = new List<string>
+        {
+            "ah", "u", "ae", "e", "i"
+        };
+        public List<string> Consanants = new List<string>
+        {
+            "p", "f", "sh", "th", "j", "r", "l", "y", "n", "m"
+        };
+    }
+}
