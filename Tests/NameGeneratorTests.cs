0d09785416ee4cf81a7fdf30bd630c1825a42dd5
JezzyDeves <17914687+JezzyDeves@users.noreply.github.com>
2021-05-25 22:14:06 -0400

    Added basic tests

--- Tests/NameGeneratorTests.cs
+++ Tests/NameGeneratorTests.cs
@@ -1,3 +1,4 @@
+using Adyanilgen;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Tests
@@ -5,6 +6,34 @@ namespace Tests
     [TestClass]
     public class NameGeneratorTests
     {
+        private NameGenerator _nameGen;
 
+        [TestInitialize]
+        public void Arrange()
+        {
+            _nameGen = new NameGenerator();
+        }
+
+        [TestMethod]
+        public string Act()
+        {
+            return _nameGen.BasicSyllable();
+        }
+
+        [TestMethod]
+        public void BasicSyllable_ShouldReturnExpectedResult()
+        {
+            var syllable = Act();
+
+            Assert.IsTrue(syllable.Contains(syllable));
+        }
+
+        [TestMethod]
+        public void BasicSyllable_ShouldFail()
+        {
+            var syllable = Act();
+
+            Assert.IsTrue(syllable.Equals(""));
+        }
     }
 }
